bc39f101d156b55837f7c4d7ed03337773df0363
natanael <natantoh@gmail.com>
2025-06-12 13:36:11 -0300

    refatoração de producer

--- iot-kafka-monitoring/producer/producer.py
+++ iot-kafka-monitoring/producer/producer.py
@@ -4,38 +4,47 @@ from kafka import KafkaProducer
 import random
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.fake_data_sensor import generate_sensor_data
 from config.settings import settings  # Importa as configurações centralizadas
 
-KAFKA_BROKER = settings.KAFKA_BROKER
-TOPIC_NAME = settings.TOPIC_NAME
+class IoTSensorProducer:
+    def __init__(self, broker, topic):
+        self.broker = broker
+        self.topic = topic
+        self.producer = self._create_kafka_producer()
+        self.count = 0
 
-def create_kafka_producer():
-    """Cria e retorna um produtor Kafka"""
-    return KafkaProducer(
-        bootstrap_servers=[KAFKA_BROKER],
-        value_serializer=lambda x: dumps(x).encode('utf-8'),
-        api_version=(2, 8, 1),  # Força versão específica
-        request_timeout_ms=10000  # Aumenta timeout
-    )
+    def _create_kafka_producer(self):
+        return KafkaProducer(
+            bootstrap_servers=[self.broker],
+            value_serializer=lambda x: dumps(x).encode('utf-8'),
+            api_version=(2, 8, 1),
+            request_timeout_ms=10000
+        )
 
-def main():
-    producer = create_kafka_producer()
-    count = 0
-    try:
-        while True:
-            sensor_data = generate_sensor_data()
-            count += 1
-            print(f"Enviando dados #{count}: {sensor_data}")
-            producer.send(TOPIC_NAME, value=sensor_data)
-            sleep(random.uniform(0.5, 2)) # Entre 0.5 e 2 segundos entre envios
-    except KeyboardInterrupt:
-        print(f"Total de mensagens enviadas: {count}")
-        print("Parando o producer...")
-    finally:
-        producer.flush()
-        producer.close()
+    def send_sensor_data(self):
+        sensor_data = generate_sensor_data()
+        self.count += 1
+        print(f"Enviando dados #{self.count}: {sensor_data}")
+        self.producer.send(self.topic, value=sensor_data)
+
+    def run(self):
+        try:
+            while True:
+                self.send_sensor_data()
+                sleep(random.uniform(0.5, 2))
+        except KeyboardInterrupt:
+            print(f"Total de mensagens enviadas: {self.count}")
+            print("Parando o producer...")
+        finally:
+            self.producer.flush()
+            self.producer.close()
 
 if __name__ == "__main__":
-    main()
+    producer = IoTSensorProducer(
+        broker=settings.KAFKA_BROKER,
+        topic=settings.TOPIC_NAME
+    )
+    producer.run()
